2f98156967a928e5c53ebae5f74162b7ffe31cc7
mcderocher <mderoche@uw.edu>
2019-01-23 17:32:02 -0800

    Added Whale and animation scripts

---
+++ VR Skill Acquisition/Assets/Scripts/WhaleRaycast.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhaleRaycast : MonoBehaviour
+{
+    public GameObject whale;
+    Animator whaleAnimator;
+
+    public bool swimming;
+    public bool moving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        whaleAnimator = whale.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (moving = true)
+        {
+            whaleAnimator.SetBool("swimming", true);
+        } else if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider.gameObject.GetComponent<isWhale>() != null)
+            {
+                whaleAnimator.SetBool("swimming", true);
+            } else
+            {
+                whaleAnimator.SetBool("swimming", false);
+            }
+        }
+        else
+        {
+            whaleAnimator.SetBool("swimming", false);
+        }
+    }
+}
