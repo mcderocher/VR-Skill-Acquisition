37d261975314b5dec7d26d6b17bd7e71e2d0a50e
mcderocher <mderoche@uw.edu>
2019-01-30 13:43:16 -0800

    Trying new code stuff

--- VR Skill Acquisition/Assets/Scripts/PlayerController.cs
+++ VR Skill Acquisition/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets;
 
 public class PlayerController : MonoBehaviour
 {
@@ -15,12 +16,34 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        /*float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 position = transform.position;
         position.x += moveHorizontal * speed * Time.deltaTime;
         position.z += moveVertical * speed * Time.deltaTime;
-        transform.position = position;
+        transform.position = position;*/
+
+        //reading the input:
+        float horizontalAxis = CrossPlatformInputManager.GetAxis("Horizontal");
+        float verticalAxis = CrossPlatformInputManager.GetAxis("Vertical");
+
+        //assuming we only using the single camera:
+
+        //camera forward and right vectors:
+        var forward = transform.forward;
+        var right = transform.right;
+
+        //project forward and right vectors on the horizontal plane (y = 0)
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        //this is the direction in the world space we want to move:
+        var desiredMoveDirection = forward * verticalAxis + right * horizontalAxis;
+
+        //now we can apply the movement:
+        transform.Translate(desiredMoveDirection * speedMeUp * Time.deltaTime);
     }
 }
